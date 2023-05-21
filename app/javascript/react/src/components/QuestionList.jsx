0a6ec4996b7297e0ac9b3d2ba38a3fd1573b382b
Saman Batool <samanbatool08@gmail.com>
2023-05-21 15:06:24 -0400

    test frontend

---
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -0,0 +1,54 @@
+import * as React from 'react'; 
+import * as ReactDOM from 'react-dom'; 
+import QuestionDetail from './QuestionDetails';
+
+const QuestionList = () => {
+  const questionList = [
+    {
+      title: "How to check if a key is present in a hash?",
+      tag: "Ruby"
+    },
+    {
+      title: "How to check if the useState hook is being used?",
+      tag: "React"
+    },
+    {
+      title: "How to check if the DOM is being loaded properly?",
+      tag: "Javascript"
+    },
+    {
+      title: "How to create a custom scope?",
+      tag: "Rails"
+    },
+    {
+      title: "When to use a class method vs instance method?",
+      tag: "Ruby"
+    },
+    {
+      title: "What is binary search?",
+      tag: "Data Structures"
+    },
+    {
+      title: "How to check if an id present in a response from API?",
+      tag: "Rails"
+    },
+    {
+      title: "How to check/test API endpoints?",
+      tag: "Rails"
+    },
+
+  ]
+  return (
+    <div>
+      <div className="row">
+        <div className="col-lg-10 mx-auto">
+          {questionList.map((question, index) => 
+            <QuestionDetail question={question} key={index}/>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default QuestionList;
--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -1,17 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom'; 
+import QuestionList from './QuestionList';
 
 const Welcome = () => {
   return (
-    <div>
-      Welcome to this rails7/react app!
+    <div className="container">
+      <h1>Welcome to this rails7/react app!</h1>
+      <p className="lead">Practice questions</p>
+
+      <QuestionList />
+
     </div>
   )
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<Welcome />, document.getElementById('welcome'))
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   ReactDOM.render(<Welcome />, document.getElementById('welcome'))
+// })
+
+const root = ReactDOM.createRoot(document.getElementById('welcome'))
+root.render(
+  <React.StrictMode>
+    <Welcome />
+  </React.StrictMode>
+)
 
 
 export default Welcome; 
